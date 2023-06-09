bacccd0ca9e7da2ab0d2ea6ea42f4e13139b162f
Giulio Davide <giuliodavide.carparelli@mia-platform.eu>
2023-06-09 13:16:51 +0200

    fix(httpInterface): validation of response

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const { omit } = require('ramda')
 const myPackage = require('./package')
 const QueryParser = require('./lib/QueryParser')
 const CrudService = require('./lib/CrudService')
+const ResultCaster = require('./lib/ResultCaster')
 const httpInterface = require('./lib/httpInterface')
 const JSONSchemaGenerator = require('./lib/JSONSchemaGenerator')
 const createIndexes = require('./lib/createIndexes')
@@ -64,6 +65,8 @@ async function registerCrud(fastify, { modelName, isView }) {
 
   fastify.decorate('crudService', model.crudService)
   fastify.decorate('queryParser', model.queryParser)
+  fastify.decorate('castResultsAsStream', model.castResultsAsStream)
+  fastify.decorate('castItem', model.castItem)
   fastify.decorate('allFieldNames', model.allFieldNames)
   fastify.decorate('jsonSchemaGenerator', model.jsonSchemaGenerator)
   fastify.decorate('jsonSchemaGeneratorWithNested', model.jsonSchemaGeneratorWithNested)
@@ -138,6 +141,7 @@ async function loadModels(fastify) {
       },
     )
     const queryParser = new QueryParser(collectionDefinition, pathsForRawSchema)
+    const resultCaster = new ResultCaster(collectionDefinition)
     const jsonSchemaGenerator = new JSONSchemaGenerator(
       collectionDefinition,
       {},
@@ -156,6 +160,8 @@ async function loadModels(fastify) {
       definition: collectionDefinition,
       crudService,
       queryParser,
+      castResultsAsStream: () => resultCaster.asStream(),
+      castItem: (item) => resultCaster.castItem(item),
       allFieldNames,
       jsonSchemaGenerator,
       jsonSchemaGeneratorWithNested,
