32f460a669be1815c668d68ee135339da2da0ab4
Giulio Davide Carparelli <giulio.davide.97@gmail.com>
2023-07-05 18:20:58 +0200

    fix: rollback lc39 to version v6 (#118)
    * Revert "fix"
    
    This reverts commit b08be7afef4939cc54f63ccaaab7d1b722c3e424.
    
    * fix(startupperformance): with broken unit tests
    
    * fix(startupperformance): lc39 to v6
    
    * updated changelog and added some comments
    
    * node to 18.16
    
    * build(Dockerfile): updated node image
    
    * style: use already available variable
    
    ---------
    
    Co-authored-by: Mia <giuliodavide.carparelli@gmail.com>
    Co-authored-by: Daniele Bissoli <daniele.bissoli@mia-platform.eu>

--- tests/httpInterface.postBulk.test.js
+++ tests/httpInterface.postBulk.test.js
@@ -589,8 +589,7 @@ tap.test('HTTP POST /bulk', async t => {
       t.strictSame(body, {
         statusCode: 400,
         error: 'Bad Request',
-        message: 'body/0/publishDate must match pattern "^\\d{4}-\\d{2}-\\d{2}(T\\d{2}:\\d{2}:\\d{2}(\\.\\d{1,3})?(Z|[+-]\\d{2}:\\d{2}))?$"',
-        code: 'FST_ERR_VALIDATION',
+        message: 'body must match pattern "^\\d{4}-\\d{2}-\\d{2}(T\\d{2}:\\d{2}:\\d{2}(\\.\\d{1,3})?(Z|[+-]\\d{2}:\\d{2}))?$"',
       })
       t.end()
     })
--- tests/httpInterface.postList.test.js
+++ tests/httpInterface.postList.test.js
@@ -351,8 +351,7 @@ tap.test('HTTP POST /', async t => {
       t.strictSame(JSON.parse(response.payload), {
         statusCode: 400,
         error: 'Bad Request',
-        message: "body/metadata must have required property 'somethingNumber'",
-        code: 'FST_ERR_VALIDATION',
+        message: "body must have required property 'somethingNumber'",
       })
       t.end()
     })
@@ -390,7 +389,7 @@ tap.test('HTTP POST /', async t => {
       t.strictSame(JSON.parse(response.payload), {
         statusCode: 400,
         error: 'Bad Request',
-        message: 'body/metadata must NOT have additional properties. Property "unexpectedField" is not defined in validation schema',
+        message: 'body must NOT have additional properties. Property "unexpectedField" is not defined in validation schema',
       })
 
       t.end()
@@ -429,8 +428,7 @@ tap.test('HTTP POST /', async t => {
       t.strictSame(JSON.parse(response.payload), {
         statusCode: 400,
         error: 'Bad Request',
-        message: "body/attachments/0 must have required property 'name', body/attachments must be object, body/attachments must match a schema in anyOf",
-        code: 'FST_ERR_VALIDATION',
+        message: "body must have required property 'name', body must be object, body must match a schema in anyOf",
       })
       t.end()
     })
@@ -468,7 +466,7 @@ tap.test('HTTP POST /', async t => {
       t.strictSame(JSON.parse(response.payload), {
         statusCode: 400,
         error: 'Bad Request',
-        message: 'body/attachments/0 must NOT have additional properties, body/attachments must be object, body/attachments must match a schema in anyOf. Property "unexpectedField" is not defined in validation schema',
+        message: 'body must NOT have additional properties, body must be object, body must match a schema in anyOf. Property "unexpectedField" is not defined in validation schema',
       })
       t.end()
     })
