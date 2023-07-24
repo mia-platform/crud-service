24d69cdf0076cafad1326b8a16ceaf30c245b7d9
Demetrio Marino <demetrio.marino@mia-platform.eu>
2023-07-24 11:00:47 +0200

    test: restored unit tests in httpInterface.getById.test.js

--- tests/httpInterface.getById.test.js
+++ tests/httpInterface.getById.test.js
@@ -50,7 +50,7 @@ const STATION_ID = STATION_DOC._id.toString()
 const MATCHING_MIR_CODE = STATION_DOC.CodiceMIR
 const NON_MATCHING_MIR_CODE = 'WrongMirCode'
 
-tap.skip('HTTP GET /<id>', async t => {
+tap.test('HTTP GET /<id>', async t => {
   const tests = [
     {
       name: 'without filter',
@@ -274,24 +274,24 @@ tap.skip('HTTP GET /<id>', async t => {
   tests.forEach(testConf => {
     const { name, found, ...conf } = testConf
 
-    t.skip(name, async t => {
+    t.test(name, async t => {
       const response = await fastify.inject({
         method: 'GET',
         url: prefix + conf.url,
         headers: getHeaders(conf),
       })
 
-      t.skip(`should return ${found ? '200' : '404'}`, t => {
+      t.test(`should return ${found ? '200' : '404'}`, t => {
         t.strictSame(response.statusCode, (found ? 200 : 404))
         t.end()
       })
 
-      t.skip('should return "application/json"', t => {
-        t.ok(/application\/json/.skip(response.headers['content-type']))
+      t.test('should return "application/json"', t => {
+        t.ok(/application\/json/.test(response.headers['content-type']))
         t.end()
       })
 
-      t.skip(`should return ${found ? 'the document' : 'NOT_FOUND_BODY'}`, t => {
+      t.test(`should return ${found ? 'the document' : 'NOT_FOUND_BODY'}`, t => {
         if (found) {
           t.strictSame(JSON.parse(response.payload), found)
         } else {
@@ -300,7 +300,7 @@ tap.skip('HTTP GET /<id>', async t => {
         t.end()
       })
 
-      t.skip('should keep the document as is in database', async t => {
+      t.test('should keep the document as is in database', async t => {
         const documents = await collection.find().toArray()
         t.strictSame(documents, fixtures)
         t.end()
@@ -311,7 +311,7 @@ tap.skip('HTTP GET /<id>', async t => {
   })
 })
 
-tap.skip('HTTP GET /<id> with string id', async t => {
+tap.test('HTTP GET /<id> with string id', async t => {
   const tests = [
     {
       name: 'without filter',
@@ -449,23 +449,23 @@ tap.skip('HTTP GET /<id> with string id', async t => {
   tests.forEach(testConf => {
     const { name, found, ...conf } = testConf
 
-    t.skip(name, async t => {
+    t.test(name, async t => {
       const response = await fastify.inject({
         method: 'GET',
         url: stationsPrefix + conf.url,
         headers: getHeaders(conf),
       })
-      t.skip(`should return ${found ? '200' : '404'}`, t => {
+      t.test(`should return ${found ? '200' : '404'}`, t => {
         t.strictSame(response.statusCode, (found ? 200 : 404))
         t.end()
       })
 
-      t.skip('should return "application/json"', t => {
-        t.ok(/application\/json/.skip(response.headers['content-type']))
+      t.test('should return "application/json"', t => {
+        t.ok(/application\/json/.test(response.headers['content-type']))
         t.end()
       })
 
-      t.skip(`should return ${found ? 'the document' : 'NOT_FOUND_BODY'}`, t => {
+      t.test(`should return ${found ? 'the document' : 'NOT_FOUND_BODY'}`, t => {
         if (found) {
           t.strictSame(JSON.parse(response.payload), found)
         } else {
@@ -474,7 +474,7 @@ tap.skip('HTTP GET /<id> with string id', async t => {
         t.end()
       })
 
-      t.skip('should keep the document as is in database', async t => {
+      t.test('should keep the document as is in database', async t => {
         const documents = await collection.find().toArray()
         t.strictSame(documents, stationFixtures)
         t.end()
@@ -485,10 +485,10 @@ tap.skip('HTTP GET /<id> with string id', async t => {
   })
 })
 
-tap.skip('HTTP GET', async t => {
+tap.test('HTTP GET', async t => {
   const { fastify, resetCollection } = await setUpTest(t, null, 'books', undefined, true)
 
-  t.skip('/:id cast correctly nested object with schema', async t => {
+  t.test('/:id cast correctly nested object with schema', async t => {
     const DOC_TEST = {
       ...fixtures[0],
       _id: ObjectId.createFromHexString('211111111111111111111111'),
@@ -526,7 +526,7 @@ tap.skip('HTTP GET', async t => {
     t.end()
   })
 
-  t.skip('/:id filter correctly by nested object field', async t => {
+  t.test('/:id filter correctly by nested object field', async t => {
     const DOC_TEST = {
       ...fixtures[0],
       _id: ObjectId.createFromHexString('311111111111111111111111'),
@@ -578,7 +578,7 @@ tap.skip('HTTP GET', async t => {
     t.end()
   })
 
-  t.skip('/:id 404 with unmatching filter on nested object', async t => {
+  t.test('/:id 404 with unmatching filter on nested object', async t => {
     const DOC_TEST = {
       ...fixtures[0],
       _id: ObjectId.createFromHexString('311111111111111111111111'),
@@ -608,11 +608,11 @@ tap.skip('HTTP GET', async t => {
     t.end()
   })
 
-  t.skip('/:id fails with 400', async t => {
+  t.test('/:id fails with 400', async t => {
     await resetCollection([DOC])
 
     UNALLOWED_RAW_PROJECTIONS.forEach(projection => {
-      t.skip('Should not allow raw projection', async assert => {
+      t.test('Should not allow raw projection', async assert => {
         const { statusCode } = await fastify.inject({
           method: 'GET',
           url: `${prefix}/${ID}?_rawp=${JSON.stringify(projection)}`,
@@ -624,7 +624,7 @@ tap.skip('HTTP GET', async t => {
       })
     })
 
-    t.skip('Should raise error if raw projection tries to override acls', async assert => {
+    t.test('Should raise error if raw projection tries to override acls', async assert => {
       const expectedPayload = {
         statusCode: 400,
         error: 'Bad Request',
@@ -644,7 +644,7 @@ tap.skip('HTTP GET', async t => {
     })
 
     UNALLOWED_RAW_PROJECTIONS.forEach(() => {
-      t.skip('should not allow raw projection with projection (mixed _rawp and _p)', async assert => {
+      t.test('should not allow raw projection with projection (mixed _rawp and _p)', async assert => {
         const expectedPayload = {
           statusCode: 400,
           error: 'Bad Request',
