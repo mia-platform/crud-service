db38a344a3f0ed06360dd0cfb5a45497f093f97e
Daniele Bissoli <daniele.bissoli@mia-platform.eu>
2025-05-28 17:43:32 +0200

    refactor(projection): move projection logic into dedicated file

---
+++ docs/projectionUtils.js
@@ -0,0 +1,172 @@
+const BadRequestError = require("../lib/BadRequestError");
+const {rawProjectionDictionary} = require("../lib/consts");
+const {isEmpty} = require("lodash");
+
+/** This function tries to extract the content of `$project` MongoDB stage
+ *  to be employed in the find operator
+ *
+ * @param {string|undefined} clientProjection a string representing a list of comma separated field names
+ * @param {string|undefined} aclColumns a string representing a list of comma separated field names that are allowed to be visualized by current user
+ * @param {Array.<string>|undefined} allFieldNames a list of fields names associated to this collection
+ * @param {string|undefined} rawProjection a string representing an object that can be employed to manipulate which or how fields should be returned
+ * @param {Logger} log a logger instance
+ * @returns {Object.<string, number|string|Object>} an object representing a projection filter for MongoDB
+ */
+function resolveProjection(
+  clientProjection,
+  aclColumns,
+  allFieldNames,
+  rawProjection,
+  log
+) {
+  log.debug('resolving projections')
+  const acls = intoArray(aclColumns)
+
+  if (clientProjection && rawProjection) {
+    log.error(
+      {_p: clientProjection, _rawp: rawProjection},
+      'use of both _p and _rawp is not permitted'
+    )
+
+    throw new BadRequestError('Use of both _rawp and _p parameter is not allowed')
+  }
+
+  let projection
+  if (!clientProjection && !rawProjection) {
+    projection = removeAclColumns(allFieldNames, acls)
+  } else if (rawProjection) {
+    projection = resolveRawProjectionString(rawProjection, acls, allFieldNames, log)
+  } else if (clientProjection) {
+    projection = resolveClientProjectionString(clientProjection, acls)
+  }
+
+  return getProjection(projection)
+}
+
+function getProjection(projection) {
+  // In case of empty projection, we project only the _id
+  if (!projection?.length) {
+    return {_id: 1}
+  }
+
+  return projection.reduce(
+    (acc, val) => {
+      const propertiesToInclude = typeof val === 'string'
+        // a string represents the name of a field to be projected
+        ? {[val]: 1}
+        // an object represents a raw projection to be passed as it is
+        : val
+
+      return {...acc, ...propertiesToInclude}
+    },
+    {}
+  )
+}
+
+function resolveClientProjectionString(clientProjectionString, _acls) {
+  const clientProjection = intoArray(clientProjectionString)
+  return removeAclColumns(clientProjection, _acls)
+}
+
+function resolveRawProjectionString(rawProjection, _acls, allFieldNames, log) {
+  try {
+    checkAllowedOperators(rawProjection, rawProjectionDictionary, _acls.length > 0 ? _acls : allFieldNames, log)
+
+    const rawProjectionObject = resolveRawProjection(rawProjection)
+    const projection = removeAclColumnsFromRawProjection(rawProjectionObject, _acls)
+
+    return !isEmpty(projection) ? [projection] : []
+  } catch (errorMessage) {
+    log.error(errorMessage.message)
+    throw new BadRequestError(errorMessage.message)
+  }
+}
+
+/** Take a string containing comma-separated values
+ *  and split it into a list of its components, removing blank elements.
+ *
+ * @param {string} value
+ * @returns {Array.<string>}
+ */
+function intoArray(value) {
+  return value ? value.split(',').filter(elem => elem.trim().length > 0) : []
+}
+
+function removeAclColumns(fieldsInProjection, aclColumns) {
+  return aclColumns.length > 0 ? fieldsInProjection.filter(field => aclColumns.includes(field)) : fieldsInProjection
+}
+
+function removeAclColumnsFromRawProjection(rawProjectionObject, aclColumns) {
+  if (checkIfRawProjectionOverridesAcls(rawProjectionObject, aclColumns)) {
+    throw Error('_rawp exclusive projection is overriding at least one acl_read_column value')
+  }
+
+  const rawProjectionFields = Object.keys(rawProjectionObject)
+  const filteredFields = removeAclColumns(rawProjectionFields, aclColumns)
+
+  return filteredFields.reduce(
+    (acc, current) => {
+      if (rawProjectionObject[current] === 0 || rawProjectionObject[current] !== undefined || rawProjectionObject[current] !== null) {
+        acc[current] = rawProjectionObject[current]
+      }
+
+      return acc
+    },
+    {}
+  )
+}
+
+function resolveRawProjection(clientRawProjectionString) {
+  return clientRawProjectionString ? JSON.parse(clientRawProjectionString) : {}
+}
+
+// to match camelCase operators, snake mongo_systems variables and field variables
+const fieldVarOrOperatorsRegex = /\${1,2}[a-zA-Z_]+/g
+
+function checkAllowedOperators(rawProjection, projectionDictionary, additionalFields, log) {
+  if (!rawProjection) {
+    log.debug('no raw projection found: checkAllowedOperators returns true')
+    return true
+  }
+
+  const {allowedOperators, notAllowedOperators} = projectionDictionary
+  const allowedFields = [...allowedOperators]
+
+  additionalFields.forEach(field => allowedFields.push(`$${field}`))
+
+  log.debug({allowedOperators: allowedFields}, 'Allowed operators for projection')
+  log.debug({notAllowedOperators}, 'Not allowed operators for projection')
+
+  const matches = rawProjection.match(fieldVarOrOperatorsRegex)
+
+  if (!matches) {
+    log.debug('No operators found in raw projection: checkAllowedOperators returns true')
+    return true
+  }
+
+  return !matches.some(match => {
+    if (match.startsWith('$$')) {
+      log.debug({match}, 'Found $$ match in raw projection')
+      if (notAllowedOperators.includes(match)) {
+        throw Error(`Operator ${match} is not allowed in raw projection`)
+      }
+
+      return notAllowedOperators.includes(match)
+    }
+
+    if (!allowedFields.includes(match)) {
+      throw Error(`Operator ${match} is not allowed in raw projection`)
+    }
+
+    return !allowedFields.includes(match)
+  })
+}
+
+function checkIfRawProjectionOverridesAcls(rawProjection, acls) {
+  return Object.keys(rawProjection).some(field => acls.includes(field) && rawProjection[field] === 0)
+}
+
+module.exports = {
+  resolveProjection,
+  checkAllowedOperators
+}
---
+++ tests/projectionUtils.test.js
@@ -0,0 +1,397 @@
+'use strict'
+
+const tap = require('tap')
+const {resolveProjection} = require('../docs/projectionUtils')
+const BadRequestError = require('../lib/BadRequestError')
+
+const loggerMock = {
+  debug: () => {
+
+    /* NOOP */
+  },
+  info: () => {
+
+    /* NOOP */
+  },
+  error: () => {
+
+    /* NOOP */
+  },
+}
+
+const DOCUMENT_FIELDS = [
+  'street',
+  'number',
+  'zipCode',
+  'city',
+  'province',
+  'state',
+  'isResidence',
+  'createdAt',
+]
+
+const RAW_PROJECTION_PLAIN_INCLUSIVE = JSON.stringify({
+  street: 1,
+  number: 1,
+  zipCode: true,
+})
+
+const RAW_PROJECTION_PLAIN_EXCLUSIVE = JSON.stringify({
+  street: 0,
+  number: 0,
+  zipCode: false,
+})
+
+tap.test('Projection Utils tests', async t => {
+  await t.test('resolveProjection', async t => {
+    await t.test('OK - no projection provided', async assert => {
+      const parsedProjection = resolveProjection(
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        loggerMock
+      )
+
+      assert.strictSame(parsedProjection, {_id: 1})
+    })
+
+    await t.test(`OK - simple without ACL`, async t => {
+      const cases = [
+        {
+          clientProjection: undefined,
+          rawProjection: undefined,
+          expected: {
+            street: 1,
+            number: 1,
+            zipCode: 1,
+            city: 1,
+            province: 1,
+            state: 1,
+            isResidence: 1,
+            createdAt: 1,
+          },
+        },
+        {
+          clientProjection: 'street,number,zipCode',
+          rawProjection: undefined,
+          expected: {street: 1, number: 1, zipCode: 1},
+        },
+        {
+          clientProjection: undefined,
+          rawProjection: RAW_PROJECTION_PLAIN_INCLUSIVE,
+          expected: {street: 1, number: 1, zipCode: true},
+        },
+        {
+          clientProjection: undefined,
+          rawProjection: RAW_PROJECTION_PLAIN_EXCLUSIVE,
+          expected: {
+            street: 0,
+            number: 0,
+            zipCode: false,
+          },
+        },
+        // {
+        //   clientProjection: undefined,
+        //   rawProjection: JSON.stringify({ 'street': '$street' }),
+        //   expected: {
+        //     street: '$street',
+        //   },
+        // },
+      ]
+
+      for (const [idx, {clientProjection, rawProjection, expected}] of cases.entries()) {
+        await t.test(`${idx}`, async assert => {
+          const parsedProjection = resolveProjection(
+            clientProjection,
+            undefined,
+            DOCUMENT_FIELDS,
+            rawProjection,
+            loggerMock
+          )
+
+          assert.strictSame(
+            parsedProjection,
+            expected
+          )
+        })
+      }
+    })
+
+    await t.test(`OK - simple with ACL`, async t => {
+      const cases = [
+        {
+          clientProjection: undefined,
+          rawProjection: undefined,
+          expected: {
+            street: 1,
+            zipCode: 1,
+          },
+        },
+        {
+          clientProjection: 'street,number,zipCode',
+          rawProjection: undefined,
+          expected: {street: 1, zipCode: 1},
+        },
+        {
+          clientProjection: undefined,
+          rawProjection: RAW_PROJECTION_PLAIN_INCLUSIVE,
+          expected: {street: 1, zipCode: true},
+        },
+        {
+          clientProjection: undefined,
+          rawProjection: JSON.stringify({'street': '$street'}),
+          expected: {
+            street: '$street',
+          },
+        },
+      ]
+
+      for (const [idx, {clientProjection, rawProjection, expected}] of cases.entries()) {
+        await t.test(`${idx}`, async assert => {
+          const parsedProjection = resolveProjection(
+            clientProjection,
+            'street,zipCode',
+            DOCUMENT_FIELDS,
+            rawProjection,
+            loggerMock
+          )
+
+          assert.strictSame(
+            parsedProjection,
+            expected
+          )
+        })
+      }
+    })
+
+    await t.test(`OK - aggregation without ACL`, async t => {
+      const cases = [
+        {
+          rawProjection: JSON.stringify({
+            city: {
+              $filter: {
+                input: '$city',
+                as: 'item',
+                cond: {
+                  $in: [
+                    '$$item.name', ['note'],
+                  ],
+                },
+              },
+            },
+          }),
+          expected: {
+            city: {
+              $filter: {
+                input: '$city',
+                as: 'item',
+                cond: {
+                  $in: [
+                    '$$item.name', ['note'],
+                  ],
+                },
+              },
+            },
+          },
+        },
+        {
+          rawProjection: JSON.stringify({
+            city: {$first: '$city'},
+          }),
+          expected: {
+            city: {$first: '$city'},
+          },
+        },
+        {
+          rawProjection: JSON.stringify({
+            createdAt: {
+              $dateToString: {
+                date: '$createdAt',
+                format: '%H',
+                // same as offset below (+ 10)
+                timezone: '+10:00',
+              },
+            },
+          }),
+          expected: {
+            createdAt: {
+              $dateToString: {
+                date: '$createdAt',
+                format: '%H',
+                // same as offset below (+ 10)
+                timezone: '+10:00',
+              },
+            },
+          },
+        },
+      ]
+
+      for (const [idx, {clientProjection, rawProjection, expected}] of cases.entries()) {
+        await t.test(`${idx}`, async assert => {
+          const parsedProjection = resolveProjection(
+            clientProjection,
+            undefined,
+            DOCUMENT_FIELDS,
+            rawProjection,
+            loggerMock
+          )
+
+          assert.strictSame(
+            parsedProjection,
+            expected
+          )
+        })
+      }
+    })
+
+    await t.test(`KO - return Bad Request due to ACL`, async t => {
+      const cases = [
+        {
+          clientProjection: undefined,
+          rawProjection: JSON.stringify({'city': '$city'}),
+          expectedError: new BadRequestError('Operator $city is not allowed in raw projection'),
+        },
+        {
+          clientProjection: undefined,
+          rawProjection: RAW_PROJECTION_PLAIN_EXCLUSIVE,
+          expectedError: new BadRequestError('_rawp exclusive projection is overriding at least one acl_read_column value'),
+        },
+      ]
+
+      for (const [idx, {clientProjection, rawProjection, expectedError}] of cases.entries()) {
+        await t.test(`${idx}`, async assert => {
+          try {
+            resolveProjection(
+              clientProjection,
+              'street,zipCode',
+              DOCUMENT_FIELDS,
+              rawProjection,
+              loggerMock
+            )
+            assert.fail()
+          } catch (error) {
+            assert.match(error, expectedError)
+          }
+        })
+      }
+    })
+
+    await t.test(`KO - operator not allowed`, async t => {
+      const cases = [
+        {
+          rawProjection: {
+            city: {$first: '$$ROOT'},
+          },
+          unwantedOperator: '$$ROOT',
+        },
+        {
+          rawProjection: {
+            city: {$first: ['$$ROOT']},
+          },
+          unwantedOperator: '$$ROOT',
+        },
+        {
+          rawProjection: {
+            city: '$$ROOT',
+          },
+          unwantedOperator: '$$ROOT',
+        },
+        {
+          rawProjection: {
+            city: '$$CURRENT',
+          },
+          unwantedOperator: '$$CURRENT',
+        },
+        {
+          rawProjection: {
+            city: [{branch: 'node'}, {leaves: '$$PRUNE'}],
+          },
+          unwantedOperator: '$$PRUNE',
+        },
+        {
+          rawProjection: {
+            $cond: {if: {val: '$$CURRENT'}, then: '$$ROOT', else: null},
+          },
+          unwantedOperator: '$$CURRENT',
+        },
+        {
+          rawProjection: {
+            remove: '$$REMOVE',
+          },
+          unwantedOperator: '$$REMOVE',
+        },
+        {
+          rawProjection: {
+            descend: '$$DESCEND',
+          },
+          unwantedOperator: '$$DESCEND',
+        },
+        {
+          rawProjection: {
+            keep: '$$KEEP',
+          },
+          unwantedOperator: '$$KEEP',
+        },
+        {
+          rawProjection: {
+            clusterTime: '$$CLUSTER_TIME',
+          },
+          unwantedOperator: '$$CLUSTER_TIME',
+        },
+        {
+          rawProjection: {
+            now: '$$NOW',
+          },
+          unwantedOperator: '$$NOW',
+        },
+        {
+          rawProjection: {
+            doc: {$concatArrays: [['$city'], ['$$ROOT']]},
+          },
+          unwantedOperator: '$$ROOT',
+        },
+        {
+          rawProjection: {
+            reduce: {
+              $reduce: {
+                rawProjection: '$city',
+                initialValue: '$$ROOT',
+                in: {
+                  count: {$sum: 1},
+                },
+              },
+            },
+          },
+          unwantedOperator: '$$ROOT',
+        },
+        {
+          rawProjection: {
+            doc: {
+              $in: ['$$CURRENT'],
+            },
+          },
+          unwantedOperator: '$$CURRENT',
+        },
+      ]
+
+
+      for (const [idx, {rawProjection, unwantedOperator}] of cases.entries()) {
+        await t.test(`${idx}`, async assert => {
+          try {
+            resolveProjection(
+              undefined,
+              undefined,
+              DOCUMENT_FIELDS,
+              JSON.stringify(rawProjection),
+              loggerMock
+            )
+            assert.fail()
+          } catch (error) {
+            assert.match(error, new BadRequestError(`Operator ${unwantedOperator} is not allowed in raw projection`))
+          }
+        })
+      }
+    })
+  })
+})
